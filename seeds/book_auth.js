6a8f03459190c429151c3d3831151214e5ff47b4
Natalie <natalie.todd15@gmail.com>
2018-08-27 15:55:52 -0600

    seeded

--- seeds/book_auth.js
+++ seeds/book_auth.js
@@ -1,10 +1,10 @@
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('book_auth').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('book_auth').insert([
         { 
           id: 1, 
           bookId: 1, 
