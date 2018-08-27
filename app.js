9373d8b482a5ae0258ed7ce14512d49d33f51ec3
Natalie <natalie.todd15@gmail.com>
2018-08-27 10:31:52 -0600

    still having issues deleting the node modules

--- app.js
+++ app.js
@@ -6,3 +6,10 @@ const port = parseInt(process.env.PORT || 3000);
 const queries = require("./queries.js");
 const cors = require("cors");
 
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(port)
+    .on("error", console.error.bind(console))
+    .on("listening", console.log.bind(console, "Listening on " + port));
