0706234c15aa9b865011759aa110bf9f9823209e
Natalie <natalie.todd15@gmail.com>
2018-08-27 11:42:35 -0600

    added migrations, about to do latest

---
+++ migrations/20180827113404_tableOfBooks.js
@@ -0,0 +1,26 @@
+
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('tableOfBooks', table => {
+        table.increments('id').primary()
+        table.text('bookTitle')
+        table.text('bookGenre')
+        table.text('bookDescription')
+        table.text('bookCoverUrl')
+        table.text('author1FirstName')
+        table.text('author1LastName')
+        table.text('author1Biography')
+        table.text('author1Portrait')
+        table.text('author2FirstName')
+        table.text('author2LastName')
+        table.text('author2Biography')
+        table.text('author2Portrait')
+        table.text('author3FirstName')
+        table.text('author3LastName')
+        table.text('author3Biography')
+        table.text('author3Portrait')
+    })
+};
+
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('tableOfBooks')
+};
