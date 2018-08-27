14530f3a8a357d6b53574dab906c46efa90ecae2
Natalie <natalie.todd15@gmail.com>
2018-08-27 14:14:58 -0600

    taking a break

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
 const bodyParser = require("body-parser");
-const morgan = require("morgan");
+// const morgan = require("morgan");
 const port = parseInt(process.env.PORT || 3000);
-const queries = require("./queries.js");
+// const queries = require("./queries.js");
 const cors = require("cors");
 
-app.use(morgan("dev"));
+// app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use(cors());
 
